526af9cd5c52d233dff1d8431dd7e8c5d65c712b
Einer Cupino <ecupino@my.centennialcollege.ca>
2024-03-30 23:23:11 -0400

    updated container color

--- src/StudentForm.jsx
+++ src/StudentForm.jsx
@@ -23,7 +23,7 @@ export default function StudentForm({ onFormSubmit }) {
       borderRadius: '5px',
       maxWidth: '500px',
       margin: 'auto',
-      backgroundColor: '#BBDEFB',
+      backgroundColor: '#81C784',
       fontFamily: 'Arial, sans-serif'
     }
   };
